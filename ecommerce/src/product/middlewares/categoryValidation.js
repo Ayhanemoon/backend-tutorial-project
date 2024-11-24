30e02af95dccaa5dfcd2962deeac18dc9ed7132e
rahmani <erahmani1369@gmail.com>
2024-11-24 11:13:13 +0330

    feat: swagger-autogen was added
    feat: category api added.
    
    fix:comment edited

---
+++ ecommerce/src/product/middlewares/categoryValidation.js
@@ -0,0 +1,52 @@
+const Category = require('../models/category');
+const {attributeValidation} = require('../middlewares/attributeValidation');
+const {check} = require('express-validator');
+const mongoose = require('mongoose');
+
+//exports.requiredValidation => check to be a valid boolean
+
+exports.categoryIdValidation = check('categoryId')
+  .custom(categoryId => mongoose.isValidObjectId(categoryId))
+  .withMessage('Category id must be an object id.')
+  .bail()
+  .customSanitizer(categoryId => mongoose.Types.ObjectId.createFromHexString(categoryId))
+  .custom(async (categoryId) => {
+    const category = await Category.findById(categoryId);
+    if (!category) {
+      throw new Error(`Category with id '${categoryId}' does not exists.`);
+    }
+    return true;
+  });
+
+exports.categoryNameValidation = check('name')
+  .notEmpty()
+  .withMessage('Enter a name for category.')
+  .bail();
+
+exports.categoryParentValidation = check('parent.parentId')
+  .optional()
+  .custom(parentId => mongoose.isValidObjectId(parentId))
+  .withMessage('Parent id must be an object id.')
+  .bail()
+  .customSanitizer(parentId => mongoose.Types.ObjectId.createFromHexString(parentId))//can add name to attribute?
+  .custom(async(parentId, {req})=>{
+    const category = await Category.findById(parentId);
+    if (!category) {
+      throw new Error(`Parent category with id '${parentId}' does not exists.`);
+    }
+    if (category.name !== req.body.parent.parentName) {
+      throw new Error('Parent name is not same as the name stored for the parent id.');
+    }
+    return true;
+  });
+
+exports.categoryAttributesValidation = check('attributes') // uniq attribute ids...
+  .optional()
+  .custom(attributes => attributes.every(async (attr) => await attributeValidation(attr.attributeId, attr.attributeName, attr.attributeValues)))
+  .customSanitizer(attributes => attributes.map(attr => ({
+    required: attr.required ?? false,
+    attributeId: mongoose.Types.ObjectId.createFromHexString(attr.attributeId),
+    attributeName: attr.attributeName,
+    attributeValues: attr.attributeValues
+  })
+  ));
