2f78f8aef6334d8925b41930308125c52422faa9
rahmani <erahmani1369@gmail.com>
2024-12-10 08:32:36 +0330

    feat: Transaction added to payment

--- ecommerce/src/payment/controllers/paymentController.js
+++ ecommerce/src/payment/controllers/paymentController.js
@@ -1,33 +1,39 @@
 const Payment = require('../models/payment');
 const SalesOrder = require('../../sales/models/salesOrder');
 const Invoice = require('../../sales/models/invoice');
+const mongoose = require('mongoose');
 
 exports.createPayment = async(req, res) => {
 // #swagger.tags = ['Payment']
+  const session = await mongoose.startSession();
   try {
     const { salesOrderId, totalPrice } = req.body;
 
-    const salesOrder = await SalesOrder.findById(salesOrderId);
+    session.startTransaction();
+    const salesOrder = await SalesOrder.findById(salesOrderId).session(session);
     if (!salesOrder) {
       throw new Error('Sales order does not exists.');
     }
     salesOrder.status = 'paid';
-    await salesOrder.save();
+    await salesOrder.save({session});
 
-    const invoice = await Invoice.findOne({'orderId': salesOrderId});
+    const invoice = await Invoice.findOne({'orderId': salesOrderId}).session(session);
     if ( !invoice.orderId.equals(salesOrderId)) {
       throw new Error('You should create invoice for sales order');
     }
     invoice.status = 'paid';
-    await invoice.save();
+    await invoice.save({session});
 
     //---payment
     const payment = new Payment({salesOrderId, totalPrice});
-    await payment.save();
-
+    await payment.save({session});
+    await session.commitTransaction();
     return res.status(201).json({ message: 'Payment created successfully', payment });
   } catch (error) {
+    session.abortTransaction();
     return res.status(500).json({ message: 'Error creating payment.', error: error });
+  } finally {
+    session.endSession();
   }
 };
 
