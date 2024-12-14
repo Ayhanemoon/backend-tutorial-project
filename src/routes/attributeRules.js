c86185bba112fc310163c1bb8cef0613e32ce57b
rahmani <erahmani1369@gmail.com>
2024-12-14 11:39:10 +0330

    fixed : remove old coldes

--- src/routes/attributeRules.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const attributeRuleController = require('../controllers/attributeRuleController');
-
-router.post('/', attributeRuleController.createOrUpdateAttributeRule);
-router.get('/', attributeRuleController.getAllAttributeRules);
-router.get('/:categoryName', attributeRuleController.getAttributeRuleByCategory);
-router.delete('/:categoryName', attributeRuleController.deleteAttributeRule);
-
-module.exports = router;
